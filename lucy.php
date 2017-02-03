ae6e90d8d5f3d00c29c032c9c7a903627e360e26
Preda Bogdan <Preda Bogdan>
2017-02-03 19:19:41 +0200

    Zbook PHPCS changes

--- lucy.php
+++ lucy.php
@@ -1,7 +1,7 @@
 <?php
 spl_autoload_register( function( $class_name ) {
-    $class_file_name = strtolower( str_replace( '_', '-', $class_name ) ) . '-class';
-    include 'lib/' . $class_file_name . '.php';
+	$class_file_name = strtolower( str_replace( '_', '-', $class_name ) ) . '-class';
+	include 'lib/' . $class_file_name . '.php';
 } );
 
 $salt = new Salt();
@@ -23,5 +23,3 @@ print_r( $res );
 
 $res = $salt->exec_salt_cmd( '/etc/init.d/httpd restart' );
 print_r( $res );
-
-?>
