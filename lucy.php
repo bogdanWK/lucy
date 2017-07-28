a1a1f2eae9b774c568b53e97c6fce5c8834db2d2
Preda Bogdan <bogdaWK@gmail.com>
2017-07-28 21:58:43 +0300

    Restart

--- lucy.php
+++ lucy.php
@@ -1,25 +1,2 @@
 <?php
-spl_autoload_register( function( $class_name ) {
-	$class_file_name = strtolower( str_replace( '_', '-', $class_name ) ) . '-class';
-	include 'lib/' . $class_file_name . '.php';
-} );
-
-$salt = new Salt();
-
-$res = $salt->restart();
-print_r( $res );
-
-$res = $salt->exec_salt_cmd( '/etc/init.d/httpd status' );
-print_r( $res );
-
-$res = $salt->exec_salt_cmd( '/etc/init.d/mysqld status' );
-print_r( $res );
-
-$res = $salt->exec_salt_cmd( '/etc/init.d/httpd stop' );
-print_r( $res );
-
-$res = $salt->exec_salt_cmd( '/etc/init.d/httpd start' );
-print_r( $res );
-
-$res = $salt->exec_salt_cmd( '/etc/init.d/httpd restart' );
-print_r( $res );
+echo 'Hello!';
