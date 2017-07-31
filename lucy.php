40f718bbafd6495e40c3fb133bd059053b1b12e7
Bogdan Preda <bogdan.preda@themeisle.com>
2017-07-31 14:30:06 +0300

    Test

--- lucy.php
+++ lucy.php
@@ -5,7 +5,7 @@ $errorHandler = new ErrorHandler(true, false);
 //$errorHandler->triggerError('Blah blah!', 'FATAL');
 //trigger_error('Fatal error', E_USER_ERROR);
 //trigger_error('Fatal error', E_USER_ERROR);
-echo 'Hello!' . (7/0);
+//echo 'Hello!' . (7/0);
 echo 'Hello!' . (7/0);
 echo 'Hello!' . (7/0);
 echo 'Hello!' . (7/0);
